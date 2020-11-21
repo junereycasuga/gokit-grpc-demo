5ec33175494eee02b54d9d0ef4343a3981f5a9af
Junerey Casuga <junerey.casuga@gmail.com>
2020-11-21 15:46:14 +0700

    simplify args

--- service/api.go
+++ service/api.go
@@ -12,10 +12,10 @@ type service struct {
 
 // Service interface
 type Service interface {
-	Add(ctx context.Context, numA float32, numB float32) (float32, error)
-	Subtract(ctx context.Context, numA float32, numB float32) (float32, error)
-	Multiply(ctx context.Context, numA float32, numB float32) (float32, error)
-	Divide(ctx context.Context, numA float32, numB float32) (float32, error)
+	Add(ctx context.Context, numA, numB float32) (float32, error)
+	Subtract(ctx context.Context, numA, numB float32) (float32, error)
+	Multiply(ctx context.Context, numA, numB float32) (float32, error)
+	Divide(ctx context.Context, numA, numB float32) (float32, error)
 }
 
 // NewService func initializes a service
@@ -25,18 +25,18 @@ func NewService(logger log.Logger) Service {
 	}
 }
 
-func (s service) Add(ctx context.Context, numA float32, numB float32) (float32, error) {
+func (s service) Add(ctx context.Context, numA, numB float32) (float32, error) {
 	return numA + numB, nil
 }
 
-func (s service) Subtract(ctx context.Context, numA float32, numB float32) (float32, error) {
+func (s service) Subtract(ctx context.Context, numA, numB float32) (float32, error) {
 	return numA - numB, nil
 }
 
-func (s service) Multiply(ctx context.Context, numA float32, numB float32) (float32, error) {
+func (s service) Multiply(ctx context.Context, numA, numB float32) (float32, error) {
 	return numA * numB, nil
 }
 
-func (s service) Divide(ctx context.Context, numA float32, numB float32) (float32, error) {
+func (s service) Divide(ctx context.Context, numA, numB float32) (float32, error) {
 	return numA / numB, nil
 }
